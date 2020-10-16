744cf5a73e1939349c2fe399a22b2a196405676d
sandeepG-hub <sandeep.gajula@talentica.com>
2020-10-16 13:22:28 +0530

    cluster.tf

---
+++ modules/cluster/iam.tf
@@ -0,0 +1,29 @@
+resource "aws_iam_role" "cluster" {
+  name = var.iam_role_name
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+resource "aws_iam_role_policy_attachment" "cluster-AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.cluster.name
+}
+resource "aws_cloudwatch_log_group" "default" {
+  name = "${var.name}-eks-cloudwatch_lg"
+
+  tags = {
+    Environment = var.Environment
+    Application = var.Application
+  }
+}
